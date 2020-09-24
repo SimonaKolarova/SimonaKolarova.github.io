551e7fb8d7c0f8820d81a6bdaa0cd4aba80bba24
Simona Kolarova <61417841+SimonaKolarova@users.noreply.github.com>
2020-09-24 23:57:01 +0100

    All round update

--- static/timeline.js
+++ static/timeline.js
@@ -1,10 +1,21 @@
-jQuery(document).ready(function() {
-    // code from: https://www.sitepoint.com/community/t/collapsible-bootstrap-panels-not-collapsing-after-another-panel-is-selected/225826/2
-    jQuery(".panel-heading").click(function() {
-        jQuery('#accordion .panel-heading').not(this).removeClass('isOpen');
-        jQuery(this).toggleClass('isOpen');
-        jQuery(this).next(".panel-collapse").addClass('thePanel');
-        jQuery('#accordion .panel-collapse').not('.thePanel').slideUp(300);
-        jQuery(".thePanel").slideToggle(800).removeClass('thePanel');
+$(function () {
+    // Smooth Scroll
+    $('.navbar a[href*=#]').bind('click', function(e){
+      var anchor = $(this);
+      $('html, body').stop().animate({
+        scrollTop: $(anchor.attr('href')).offset().top
+      }, 1000);
+      e.preventDefault();
     });
-});
+  });
+  
+  jQuery(document).ready(function() {
+      // code from: https://www.sitepoint.com/community/t/collapsible-bootstrap-panels-not-collapsing-after-another-panel-is-selected/225826/2
+      jQuery(".panel-heading").click(function() {
+          jQuery('#accordion .panel-heading').not(this).removeClass('isOpen');
+          jQuery(this).toggleClass('isOpen');
+          jQuery(this).next(".panel-collapse").addClass('thePanel');
+          jQuery('#accordion .panel-collapse').not('.thePanel').slideUp(300);
+          jQuery(".thePanel").slideToggle(800).removeClass('thePanel');
+      });
+  });
